2d13dccad71a471dbce48712fd33ba63081210ed
Anna Strzała <strzala.ania@gmail.com>
2018-12-11 17:27:30 +0100

    Add progess bar & restart button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 var elem = document.querySelector('.main-carousel');
+var progressBar = document.querySelector('.progress-bar');
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.restart-button');
+
 var flkty = new Flickity(elem, {
     // options
     imagesLoaded: true,
@@ -6,4 +10,22 @@ var flkty = new Flickity(elem, {
     pageDots: false,
     contain: true,
     hash: true,
+});
+
+// Use progress bar scroll
+
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
+
+buttons = fizzyUIUtils.makeArray(buttons);
+buttonGroup.addEventListener('click', function (event) {
+    // filter for button clicks
+    if (!matchesSelector(event.target, '.restart-button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
 });
